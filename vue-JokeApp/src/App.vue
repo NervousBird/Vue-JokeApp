d29bc2c164d32476895322a54cdf3dc8430bae72
Leo Walton <leowaltondesign@gmail.com>
2025-07-03 14:58:43 +0200

    Basic Setup for JokeApp

---
+++ vue-JokeApp/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/newjokes">New Jokes!</RouterLink>
+        <RouterLink to="/favourites">Favourites</RouterLink>
+        <RouterLink to="/myjokes">My Jokes!</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-JokeApp/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const jokeData = ref(null);
+
+// Simple GET request using fetch
+const fetchData = () => {
+  fetch('https://v2.jokeapi.dev/joke/Any?format=json')
+    .then(response => response.json())
+    .then(data => jokeData.value = data.setup + data.delivery);
+}
+
+</script>
+
+<template>
+  <main>
+    <h1>Home!</h1>
+
+    <div>
+      <p>{{ jokeData }}</p>
+      <button @click="fetchData">Get API Info</button>
+    </div>
+  </main>
+</template>
